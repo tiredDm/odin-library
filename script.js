837c477b9ed756fc9c140b10a11f984771fe45a3
tiredDm <ronpri1@gmail.com>
2023-10-19 12:14:37 -0400

    Base Skeleton for scaffolding

---
+++ script.js
@@ -0,0 +1,10 @@
+//interesting Styling Idea --> Make the UI similar to that of an actual bookshelf... Have active variables like, colour, text font, and 
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
