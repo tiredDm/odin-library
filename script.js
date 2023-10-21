1bc4cf5206b1e6feae3e17afc456c067a47d5ee4
tiredDm <ronpri1@gmail.com>
2023-10-21 11:46:41 -0400

    begun script and form work

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 //interesting Styling Idea --> Make the UI similar to that of an actual bookshelf... Have active variables like, colour, text font, and 
 const myLibrary = [];
+const container = document.querySelector('.container');
+const shelves = document.querySelector('shelves');
 
-function Book() {
-  // the constructor...
+function Book(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  //We might want to have a some additional attributes for styling later.
 }
 
 function addBookToLibrary() {
   // do stuff here
+  let newBook = document.createElement('div');
+  newBook.classList.add('book');
+  shelves[0].appendChild(newBook);
+  //  myLibrary.push(newBook)
+
+}
+
+function expandbookShelves(){
+
 }
