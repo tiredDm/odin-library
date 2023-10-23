451d58eea5b4acb3e8800bd90a79224f0a255dd4
tiredDm <ronpri1@gmail.com>
2023-10-23 11:54:31 -0400

    Added Book Customization on Submit

--- script.js
+++ script.js
@@ -15,11 +15,37 @@ function Book(author, title, pages) {
 
 
 function addBookToLibrary() {
-  // do stuff here
+  // GATHER ALL VALUES HERE
+  let author = document.getElementById('Author');
+  let title = document.getElementById('Title');
+  let pages = document.getElementById('Pages');
+ 
+  let page = 250; // if smaller than 100, make 100
+  if (pages.value < 100){
+    pages.value = 100;
+  }
+
+  console.log(author.value + ',' + title.value + ',' + pages.value) ;
+
+
+  //CREATE OBJECT
+
+
+  //DO ALL THE STUFF
   let newBook = document.createElement('div');
   newBook.classList.add('book');
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  let width = pages.value*.01 +'rem';
+  let height = 60+(Math.random()*33) + '%';
+  newBook.style.backgroundColor = randomColor;
+  newBook.style.width = width; // Page contributes to this..
+  newBook.style.height = height;
+
   currentShelf.appendChild(newBook);
   //  myLibrary.push(newBook)
+
+  //CLEAR ALL THE FORM VALUES 
+
 
 }
 
@@ -31,3 +57,13 @@ function expandbookShelves(){
 btn.addEventListener('click', () => {
   addBookToLibrary();
 });
+
+
+/* 
+Check List...
+1. Object to Style -> take object properties and put them into the style..
+  1a. What are the Styles we want... Width of the book, Colour, Height, Pattern, Title Font. 
+2. take form inputs and read them into the object
+3. Validate Form Inputs (If in the Odin Project)
+4. Implement Move Row and Expand bookShelves..
+*/
